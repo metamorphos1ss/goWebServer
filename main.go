979dc9b5adb04e69f555df6d250632767e3e80f0
metamorphos1ss <414metamorphosis@gmail.com>
2025-03-14 00:08:25 +0300

    .

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 
 func getHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got /hello request\n")
-	io.WriteString(w, "Heyo, HTTP!\n")
+	io.WriteString(w, "Hello, HTTP!\n")
 }
 
 func main() {
